56e67ab749a1900138f9e907a537cf1d861db0ad
Partho Paul <partho8897@gmail.com>
2018-06-14 21:06:56 +0530

    pull

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ export default class App extends Component {
         <a class="dropdown-item" href="#">Action</a>
         <a class="dropdown-item" href="#">Another action</a>
         <a class="dropdown-item" href="#">Something else here</a>
-        <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="#">Separated link</a>
       </div>
     </div>
