cce9382b44cee4ed71f102caeae3839604a288c5
Partho Paul <partho8897@gmail.com>
2018-06-14 17:32:19 +0530

    Buttons bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
+
+export default class App extends Component {
   render() {
     return (
-      <div>
+      <div class="btn-group">
+      <button type="button" class="btn btn-danger">Action</button>
+      <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="sr-only">Toggle Dropdown</span>
+      </button>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Separated link</a>
       </div>
+    </div>
     );
   }
 }
