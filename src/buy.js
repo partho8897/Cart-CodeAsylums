21b8aa2120874070dd3c147a2670c69a880d0e6d
Nilesh <nil@gmail.com>
2018-08-07 13:36:26 +0530

    Routing done

--- src/buy.js
+++ src/buy.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
 export default class buy extends Component{
     render(){
         return(
-            <div>l</div>
+            <div></div>
         );
     }
 }
